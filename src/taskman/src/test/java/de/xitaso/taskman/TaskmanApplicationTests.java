9c24996043ec77497c86c75b0c7c8ee7a512de58
Alexander Rampp <alexander.rampp@xitaso.com>
2022-03-01 13:40:23 +0100

    Add test configuration with proper test config

--- src/taskman/src/test/java/de/xitaso/taskman/TaskmanApplicationTests.java
+++ src/taskman/src/test/java/de/xitaso/taskman/TaskmanApplicationTests.java
@@ -1,30 +1,30 @@
 package de.xitaso.taskman;
 
-import java.util.Collection;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
 import de.xitaso.taskman.model.Project;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TaskmanApplicationTests {
 
-    @LocalServerPort
-    private int port;
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void getProjects() {
-        ResponseEntity<Project[]> projects = this.restTemplate.getForEntity("http://localhost:" + port + "/projects",
+    public void createProject() {
+        // var project = new Project("testProject");
+        // project.setDeadline(LocalDate.of(2022, 3, 1));
+        // project.setDescription("Project description");
+        // this.restTemplate.getRootUri()
+
+        ResponseEntity<Project[]> projects = this.restTemplate.getForEntity("/projects",
                 Project[].class);
         assertThat(projects.getBody().length).isEqualTo(2);
     }
