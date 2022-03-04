2d808d1a0a936d3d8fa5b9573f52880617fe002c
Alexander Rampp <alexander.rampp@xitaso.com>
2022-03-04 09:18:23 +0100

    Fix compile error with user

--- src/taskman/src/main/java/de/xitaso/taskman/entities/Task.java
+++ src/taskman/src/main/java/de/xitaso/taskman/entities/Task.java
@@ -5,19 +5,10 @@ import de.xitaso.taskman.data.EntityBase;
 public class Task extends EntityBase {
     private String description;
     private TaskState state;
-    private User assignee;
 
     public Task(String description) {
         this.description = description;
         this.setState(TaskState.ToDo);
-    }
-
-    public void assign(User user) {
-        assignee = user;
-    }
-
-    public User getAssignee() {
-        return assignee;
     }
 
     public TaskState getState() {
