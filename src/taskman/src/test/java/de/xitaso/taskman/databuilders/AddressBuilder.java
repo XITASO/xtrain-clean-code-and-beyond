8d85dadbee13168c7e6873ae7161317be8a91d92
Alexander Rampp <alexander.rampp@xitaso.com>
2022-03-05 08:03:28 +0100

    Add test data builder for address

---
+++ src/taskman/src/test/java/de/xitaso/taskman/databuilders/AddressBuilder.java
@@ -0,0 +1,20 @@
+package de.xitaso.taskman.databuilders;
+
+import de.xitaso.taskman.entities.Address;
+
+public class AddressBuilder extends DataBuilder<Address, AddressBuilder> {
+
+    public AddressBuilder InMunich() {
+        return With(a -> a.setCity("Munich"));
+    }
+
+    @Override
+    protected Address createInstance() {
+        var address = new Address();
+        address.setStreet("Some Street");
+        address.setStreetNumber("5a");
+        address.setZipCode("86167");
+        address.setCity("Augsburg");
+        return address;
+    }
+}
---
+++ src/taskman/src/test/java/de/xitaso/taskman/databuilders/DataBuilder.java
@@ -0,0 +1,23 @@
+package de.xitaso.taskman.databuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public abstract class DataBuilder<TEntity, TBuilder extends DataBuilder<TEntity, TBuilder>> {
+
+    private final List<Consumer<TEntity>> configurations = new ArrayList<>();
+
+    public TBuilder With(Consumer<TEntity> applyConfig) {
+        configurations.add(applyConfig);
+        return (TBuilder) this;
+    }
+
+    public TEntity Build() {
+        var instance = createInstance();
+        configurations.forEach(applyConfig -> applyConfig.accept(instance));
+        return instance;
+    }
+
+    protected abstract TEntity createInstance();
+}
